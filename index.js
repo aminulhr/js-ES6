6775c8be136467050a5dada708b292b4f30aeb31
aminulhr <aminulhaquerana1@gmail.com>
2024-10-07 23:12:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const x = 4;
+let t = x;
+t = 10;
+console.log('x = ',x)
+console.log('t = ',t)
+
+// false value => false,0,"",null, undefined,NaN
+// truthy value = true, 'rana', 1234, 1, '1', [],{}
+
+const value = { };
+
+// truthy value  or false value বের করার জন্য ২টা নট !!  চিহ্ন ব্যাবহার করতে হবে।
+
+console.log(!!value)
