3a5b5914c7aa486360cece513ed9837df575c2bc
aminulhr <aminulhaquerana1@gmail.com>
2024-10-08 17:34:56 +0600

    array of object

---
+++ chain.js
@@ -0,0 +1,5 @@
+const data = [{id: 1, name: 'HP', price: 24000}, {id: 1, name:'makbook', price: 120000}
+]
+console.log(data.name) //undefined  আসবে কারন এটা arrray of object
+console.log(  data[0].price) 
+console.log(data[1].price) 
