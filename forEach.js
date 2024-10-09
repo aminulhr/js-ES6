2aeda83a36196d9efceb5071a4895c1284b6a982
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-10-09 12:35:05 +0600

    for Each Funtion

--- forEach.js
+++ forEach.js
@@ -1,1 +1,5 @@
 //for Each এর কাজ map মতোই কিন্তু map return করে for each return করেনা।  এর কাজ হলো কোনো একটা array ar প্রত্যেকটা উপাদানের উপর একটা করে ফাংশন চালাবে। কিন্তু রিটার্ন করবে না।
+
+const num1 = [23,32,45,65,7,6,5,6]
+const getNum1 = num1.forEach(num1 => num1 + 10)
+console.log(getNum1)
