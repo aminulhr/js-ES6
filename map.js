c53a4360824af4cc9b641113720263f9a1d7511d
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-10-09 12:20:53 +0600

    map funtion

--- map.js
+++ map.js
@@ -1,1 +1,54 @@
 //map এর কাজ হলো কোনো একটা array ar প্রত্যেকটা উপাদানের উপর একটা করে ফাংশন চালাবে। তারপর সেগুলো array আকারে আউটপুট দিবে।
+
+const eidSalami = [150,250,150,250,350,450]
+const bonus = eidSalami.map(eidSalami => eidSalami+50)
+const shareSalami = eidSalami.map(eidSalami=> eidSalami-100)
+console.log(bonus)
+console.log(shareSalami)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
