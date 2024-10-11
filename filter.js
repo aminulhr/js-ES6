aa0836105f3bddac8a4e63bd5c60cbf873e12c2a
Aminul Haqur Rana <aminulhaquerana1@gmail.com>
2024-10-11 23:22:59 +0600

    update

--- filter.js
+++ filter.js
@@ -3,3 +3,9 @@ const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
 const result = words.filter((word) => word.length > 6);
 
 console.log(result);
+
+const words2 = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result2 = words.filter((word2) => word2.length > 6);
+
+console.log(result2);
