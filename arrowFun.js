e3e43d533b1046a6b1f2e9dac058aec6aa97bb02
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-12 23:34:49 +0600

    type of

--- arrowFun.js
+++ arrowFun.js
@@ -1,3 +1,3 @@
- const add = (a,b) => a + b;
-let sub= add(34,31)
- console.log(sub)
+const add = (a, b) => a + b;
+let sub = add(34, 31);
+console.log(sub);
---
+++ typeOF.js
@@ -0,0 +1,8 @@
+let num = "23";
+// console.log(typeof num);
+let num2 = [1, 23, 54, 5, 5, 6, 6, 3443, 434];
+// console.log(typeof nums);
+// console.log(Array.isArray(num2)); // true
+// console.log(Array.isArray(num)); // false
+let one = isNaN([3]);
+console.log(one);
