47daaed0e6f1df27ed623a3c5b93666b5c42ac6a
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-14 23:39:54 +0600

    spreed Oparator

--- spreedOprator.js
+++ spreedOprator.js
@@ -1,5 +1,6 @@
 //spreed Operator
 const numbers = [11, 22, 33, 44, 55, 66, 77, 78, 88];
 // console.log(Math.max(numbers)); // give me NaN
+console.log(Math.max(...numbers)); // give me Numbers
 // console.log(numbers); // give me full array
 // console.log(...numbers); // give me only numbers
