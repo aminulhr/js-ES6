2bb8b87cf8d2f55f5e51285d0887acff35cf7f94
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-15 00:10:44 +0600

    Destructure

---
+++ Destructure.js
@@ -0,0 +1,9 @@
+// object Destructure
+const book = {
+  name: "Time Management",
+  writer: "Ismail Kamdar",
+  page: "150",
+  price: 130,
+};
+const { name, writer, page, price } = book;
+console.log(page);
