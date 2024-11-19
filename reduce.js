55d8b8deaf3c96a2bb40c327e064495b00f9178e
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-19 23:39:26 +0600

    reduce

--- reduce.js
+++ reduce.js
@@ -1,8 +1,9 @@
-// সব গুলোকে একসাথে নিয়ে কিছু একটা করবে। 
-const number = [ 22, 44,5,6,6,7,8,5,4]
-const total = number.reduce((previous, current) => previous + current,0)
-console.log(total)
+// সব গুলোকে একসাথে নিয়ে কিছু একটা করবে।
+const number = [22, 44, 5, 6, 6, 7, 8, 5, 4];
+const total = number.reduce((previous, current) => previous + current, 0);
+console.log(total);
 
-const num2 = [34,43,5,5,6,8,9,23]
-const sum = num2.reduce((a,b)=>a+b,0)
-console.log(sum)
+const num2 = [34, 43, 5, 5, 6, 8, 9, 23];
+// 2 ta parameter dite hobe, 1 ta hole previous value ar onno ta holo current value
+const sum = num2.reduce((a, b) => a + b, 0);
+console.log(sum);
