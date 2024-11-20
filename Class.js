0ff2cab15520120b11008ea8485de7656935b0db
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-20 18:09:20 +0600

    class

--- Class.js
+++ Class.js
@@ -1,8 +1,17 @@
 // class ar nam Capital latter diya suru hobe
-const products = [
-  { id: 1, name: "toshiba", price: 50000 },
-  { id: 2, name: "asus", price: 50000 },
-  { id: 3, name: "mac", price: 50000 },
-  { id: 4, name: "dell", price: 50000 },
-  { id: 5, name: "hp", price: 50000 },
-];
+class Vehical {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  move() {
+    console.log("gari chole na ");
+  }
+}
+class Bus extends Vehical {
+  constructor(name, price, seat, ticket) {
+    super(name, price);
+    this.seat = seat;
+    this.ticket = ticket;
+  }
+}
