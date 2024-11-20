f2bebfff4c7ea673635dee90b17b71ca9989bd32
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-20 12:47:36 +0600

    class

---
+++ Class.js
@@ -0,0 +1,8 @@
+// class ar nam Capital latter diya suru hobe
+const products = [
+  { id: 1, name: "toshiba", price: 50000 },
+  { id: 2, name: "asus", price: 50000 },
+  { id: 3, name: "mac", price: 50000 },
+  { id: 4, name: "dell", price: 50000 },
+  { id: 5, name: "hp", price: 50000 },
+];
