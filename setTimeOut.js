64a1e05d8f1ed1e085baa617073d065987e40435
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-11-26 13:09:35 +0600

    set time out

---
+++ setTimeOut.js
@@ -0,0 +1,11 @@
+//settimeout 1 bar exicute hobe
+console.log(1);
+console.log(2);
+setTimeout(() => {
+  console.log(3);
+}, 3000);
+console.log(4);
+
+// function practice() {
+//   console.log(3);
+// }
